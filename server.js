f3979a92ccaa8f6e1b57fb9f0a8f4b6f7ebe523a
CaupoHelvik <helvikcaupo@gmail.com>
2019-09-08 12:56:17 +0300

    test

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const PORT = 3000
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
 app.use(express.static('public'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Server started", PORT);
+});
