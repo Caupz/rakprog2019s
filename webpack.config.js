b60e0a642164eca31f7a77063c5a22eb88bf56f2
Caupo <helvikcaupo@gmail.com>
2019-09-11 16:04:55 +0300

    config test 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
-{
-  "entry": "./src/index.js",
-  "plugins": [
-    new CopyPlugin([
-      {
-        from: "public"
-      }
-    ])
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname. "dist"),
+      filename: 'bundle.js'
+    },
+    "plugins": [
+        new CopyPlugin([
+            {
+                from: "public"
+            }
+        ])
 ]
 }
