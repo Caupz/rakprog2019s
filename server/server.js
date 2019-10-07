eb9fd90e4fbff7afd2dfd2dfd19d7378566700c6
CaupoHelvik <helvikcaupo@gmail.com>
2019-10-07 20:55:35 +0300

    lesson 5 mongodb routerid, post ja get item items

--- server/server.js
+++ server/server.js
@@ -5,44 +5,10 @@ const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
 const mongoose = require("mongoose");
 require('dotenv').config();
-
-var kittySchema = new mongoose.Schema({
-   name: String
-});
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-const kitten1 = new Kitten({
-   name: "red cat 1"
-});
-
+const userRouter = require("./user.js");
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-zsibm.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(DB_URL)
-    .then(() => {
-        console.log("DB Access success");
-        kitten1.save(err => {
-            if(err) {
-                console.log("Kitten error: ", err);
-            } else {
-                console.log("Kitten save success.");
-            }
-        });
-    })
-    .catch(err => {
-        console.log("DB Access error: ", err);
-    });
-
-app.get("/api/items", (req, res) => {
-    res.json(DB.getItems());
-});
-
-app.get("/api/items/:itemId", (req, res) => {
-    res.send(DB.getItem(req.params.itemId));
-});
-
-app.post("/hello", (req, res) => {
-    res.send("post world");
-});
+app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
@@ -54,10 +20,21 @@ app.get('/items/*', (req, res) => {
 
 app.use(express.static('dist'));
 
-app.listen(PORT, () => {
-    console.log("Server started", PORT);
-    console.log(`http://localhost:${PORT}`);
-});
+function listen() {
+    app.listen(PORT, () => {
+        console.log("Server started", PORT);
+        console.log(`http://localhost:${PORT}`);
+    });
+}
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("DB Access success");
+        listen();
+    })
+    .catch(err => {
+        console.log("DB Access error: ", err);
+    });
 /*
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://caupo:Solarion1@cluster0-zsibm.gcp.mongodb.net/test?retryWrites=true&w=majority";
