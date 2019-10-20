4599fd9aefb1bff109c07bf35bbfb92934edc6a2
CaupoHelvik <helvikcaupo@gmail.com>
2019-10-20 17:13:10 +0300

    yl5

--- server/auth.router.js
+++ server/auth.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("./user.model.js");
 const userController = require("./user.controller.js");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 const validationMiddleware = (req, res, next) => {
     const errors = validationResult(req);
@@ -11,6 +12,29 @@ const validationMiddleware = (req, res, next) => {
     }
     next();
 };
+
+router.post("/verify", (req, res) => {
+    const bearerHeader = req.headers["authorization"];
+    if(!bearerHeader) {
+        console.log("jwt verify error 1");
+        return res.send(400);
+    }
+    const token = bearerHeader.split(" ")[1];
+    if(!token) {
+        console.log("jwt verify error 2");
+        return res.send(400);
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+        if(err) {
+            console.log("jwt verify error 3", err);
+            return res.status(401).send(err);
+        }
+
+        console.log("decoded", decoded);
+        res.status(200).send(decoded);
+    });
+});
 
 router.post("/login", userController.login);
 router.post("/signup", [
